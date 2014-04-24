1270caca1cfa6e247bda6f9c7de6e4eae774e661
David John <djohn@arch-no.org>
2014-04-24 12:10:35 -0500

    updates

--- app/controllers/ano_newsletter/application_controller.rb
+++ app/controllers/ano_newsletter/application_controller.rb
@@ -5,8 +5,6 @@ class AnoNewsletter::ApplicationController < ApplicationController
   private
 
   def auth
-    p ano_newsletter_current_user
-    p ano_newsletter_authorize!
     # ano_newsletter_current_user = true if Rails.env.development?
     if ano_newsletter_current_user || ![:index, :show].include?(action_name.to_sym)
       ano_newsletter_authorize!
